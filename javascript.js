0cd043359029ecacb029e9e092fff8fb379122ec
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-08 08:13:44 -0300

    Add functions that populate the display when numbers are clicked

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const ac = document.querySelector('.ac');
+const ce = document.querySelector('.ce');
+
+let currentValue = 0;
+let displayValue = 0;
+
 function add(a, b) {
     return a + b;
 }
@@ -19,3 +27,25 @@ function div(a, b) {
 function operate(operation, a, b) {
     return operation(a, b);
 }
+
+function pressNumber(e) {
+    displayValue = Number(displayValue + e.target.textContent);
+    display.textContent = displayValue;
+}
+
+function clearAll() {
+    currentValue = 0;
+    displayValue = 0;
+    display.textContent = displayValue;
+}
+
+function clearEntry() {
+    displayValue = 0;
+    display.textContent = displayValue;
+}
+
+numbers.forEach(number => number.addEventListener('click', pressNumber));
+ac.addEventListener('click', clearAll);
+ce.addEventListener('click', clearEntry);
+
+display.textContent = displayValue;
