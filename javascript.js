1d49dbd8fe45419b6f480a2525c6d15eba7d1f01
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-08 06:47:28 -0300

    Implement basic math functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 "use strict";
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function prod(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function operate(operation, a, b) {
+    return operation(a, b);
+}
