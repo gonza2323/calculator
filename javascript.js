468531f6dba920c87c2ef96316ec1a52bde4f329
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-09 12:38:15 -0300

    Implement calculator logic

--- javascript.js
+++ javascript.js
@@ -4,9 +4,17 @@ const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 const ac = document.querySelector('.ac');
 const ce = document.querySelector('.ce');
+const addButton = document.querySelector('.add');
+const subButton = document.querySelector('.sub');
+const prodButton = document.querySelector('.prod');
+const divButton = document.querySelector('.div');
+const mod = document.querySelector('.mod');
+const equal = document.querySelector('.equal');
+const decimal = document.querySelector('.decimal');
 
 let currentValue = 0;
 let displayValue = 0;
+let currentOp = add;
 
 function add(a, b) {
     return a + b;
@@ -24,10 +32,6 @@ function div(a, b) {
     return a / b;
 }
 
-function operate(operation, a, b) {
-    return operation(a, b);
-}
-
 function pressNumber(e) {
     displayValue = Number(displayValue + e.target.textContent);
     display.textContent = displayValue;
@@ -36,6 +40,7 @@ function pressNumber(e) {
 function clearAll() {
     currentValue = 0;
     displayValue = 0;
+    currentOp = null;
     display.textContent = displayValue;
 }
 
@@ -44,8 +49,20 @@ function clearEntry() {
     display.textContent = displayValue;
 }
 
+function operate(operation) {
+    currentValue = currentOp(currentValue, displayValue);
+    display.textContent = currentValue;
+    currentOp = operation;
+    displayValue = 0;
+}
+
 numbers.forEach(number => number.addEventListener('click', pressNumber));
 ac.addEventListener('click', clearAll);
 ce.addEventListener('click', clearEntry);
+addButton.addEventListener('click', () => operate(add));
+subButton.addEventListener('click', () => operate(sub));
+prodButton.addEventListener('click', () => operate(prod));
+divButton.addEventListener('click', () => operate(div));
+equal.addEventListener('click', () => operate(add));
 
 display.textContent = displayValue;
