04a3996bbcec9f31fcf78570d3d6baf876405224
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-09 22:17:02 -0300

    Finish all calculator logic and implement decimals

--- javascript.js
+++ javascript.js
@@ -9,12 +9,15 @@ const subButton = document.querySelector('.sub');
 const prodButton = document.querySelector('.prod');
 const divButton = document.querySelector('.div');
 const modButton = document.querySelector('.mod');
-const equal = document.querySelector('.equal');
+const equalButton = document.querySelector('.equal');
 const decimal = document.querySelector('.decimal');
 
 let currentValue = 0;
 let displayValue = 0;
-let currentOp = add;
+let currentOp = equal;
+let awaitingArgument = true;
+let displayErase = false;
+
 
 function add(a, b) {
     return a + b;
@@ -36,15 +39,35 @@ function mod(a, b) {
     return a % b;
 }
 
+function equal(a, b) {
+    return b;
+}
+
 function pressNumber(e) {
-    displayValue = Number(displayValue + e.target.textContent);
+    if (awaitingArgument || displayErase) {
+        display.textContent = '';
+        displayErase = false;
+        awaitingArgument = false;
+    }
+    displayValue = Number(display.textContent + e.target.textContent);
     display.textContent = displayValue;
+}
+
+function addDecimal() {
+    if (awaitingArgument || displayErase) {
+        displayValue = 0;
+        display.textContent = 0;
+        displayErase = false;
+        awaitingArgument = false;
+    }
+    if (display.textContent.includes('.')) return;
+    display.textContent += '.';
 }
 
 function clearAll() {
     currentValue = 0;
     displayValue = 0;
-    currentOp = add;
+    currentOp = equal;
     display.textContent = displayValue;
 }
 
@@ -54,17 +77,23 @@ function clearEntry() {
 }
 
 function operate(e, operation) {
-    if (currentOp === div && displayValue === 0) {
+    if (awaitingArgument) {
         clearAll();
-        display.textContent = 'MATHERROR';
+        display.textContent = 'Missing Arg';
         return;
     }
+
     currentValue = currentOp(currentValue, displayValue);
-    display.textContent = currentValue
-    if (e.target.textContent !== '=')
-        display.textContent += e.target.textContent;
+    displayValue = currentValue;
+    display.textContent = displayValue;
+    
     currentOp = operation;
-    displayValue = 0;
+    
+    if (currentOp !== equal) {
+        awaitingArgument = true;
+        display.textContent += e.target.textContent;
+    }
+    displayErase = true;
 }
 
 numbers.forEach(number => number.addEventListener('click', pressNumber));
@@ -75,6 +104,7 @@ subButton.addEventListener('click', (e) => operate(e, sub));
 prodButton.addEventListener('click', (e) => operate(e, prod));
 divButton.addEventListener('click', (e) => operate(e, div));
 modButton.addEventListener('click', (e) => operate(e, mod));
-equal.addEventListener('click', (e) => operate(e, add));
+equalButton.addEventListener('click', (e) => operate(e, equal));
+decimal.addEventListener('click', addDecimal);
 
 display.textContent = displayValue;
